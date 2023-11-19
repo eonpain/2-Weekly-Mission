f68e5daa4b9630d927c3ef067c5bc0f50a46f093
eonpain <zmfltmwnsrl@naver.com>
2023-11-20 00:45:14 +0900

    Weekly Mission week5

--- signin.js
+++ signin.js
@@ -1,10 +1,11 @@
-let inputfocus = document.querySelector("#email-input");
-let emailform = document.querySelector("#email-form");
-let email_null = document.createElement("p");
-let email_frame_wrong = document.createElement("p");
-let exist_email = document.createElement("p");
-let emailframe = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
-let email_pwd = document.querySelector("#email-pwd");
+const inputfocus = document.querySelector("#email-input");
+const emailform = document.querySelector("#email-form");
+const email_null = document.createElement("p");
+const email_frame_wrong = document.createElement("p");
+const exist_email = document.createElement("p");
+const emailframe = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+const email_pwd = document.querySelector("#email-pwd");
+const newURL = "index.html";
 
 inputfocus.addEventListener("focusout", emailfocusout);
 
@@ -34,21 +35,6 @@ function emailfocusout() {
       email_frame_wrong.innerHTML = "올바른 이메일 주소가 아닙니다";
       email_frame_wrong.classList.add("email-error-text");
       emailform.append(email_frame_wrong);
-    }
-  } else if (inputfocus.value === "test@codeit.com") {
-    if (inputfocus.nextElementSibling !== null) {
-      let lastChild = emailform.lastChild;
-      emailform.removeChild(lastChild);
-      let exist_email = document.createElement("p");
-      exist_email.innerHTML = "이미 사용 중인 이메일입니다";
-      exist_email.classList.add("email-error-text");
-      emailform.append(exist_email);
-    } else {
-      inputfocus.style.borderColor = "var(--red)";
-      let exist_email = document.createElement("p");
-      exist_email.innerHTML = "이미 사용 중인 이메일입니다";
-      exist_email.classList.add("email-error-text");
-      emailform.append(exist_email);
     }
   } else if (inputfocus.nextElementSibling !== null) {
     let lastChild = emailform.lastChild;
@@ -80,9 +66,47 @@ function loginpass(event) {
   event.preventDefault();
   if (
     inputfocus.value === "test@codeit.com" &&
-    email_pwd.value === "codeit101"
+    email_pwd.value === "sprint101"
   ) {
-    let newURL = "index.html";
-    location.href = newURL;
+    const newMember = {
+      email: "test@codeit.com",
+      password: "sprint101",
+    };
+
+    const signInEndpoint = "https://bootcamp-api.codeit.kr/api/sign-in";
+
+    fetch(signInEndpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newMember),
+    })
+      .then((response) => response.json())
+      .then((make) => {
+        console.log(make.data.accessToken);
+        const signuptoken = make.data.accessToken;
+        localStorage.setItem("accessToken", signuptoken);
+        const result = localStorage.getItem("accessToken");
+    if (result) 
+        // 성공적으로 로그인되었으므로 "/folder" 페이지로 이동
+        {let newURL = "index.html";
+        location.href = newURL;}
+        return;
+      });
   }
 }
+
+// const newMember = {
+//   email: "test@codeit.com",
+//   password: "sprint101",
+// };
+
+// const signInEndpoint = "https://bootcamp-api.codeit.kr/api/sign-in";
+
+// fetch(signInEndpoint, {
+//   method: "POST",
+//   body: JSON.stringify(newMember),
+// })
+//   .then((response) => response.text())
+//   .then((result) => {console.log(result);});
